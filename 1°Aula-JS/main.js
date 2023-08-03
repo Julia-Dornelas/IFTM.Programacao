392d855e36e1075ff5bfc0a5d57132e4e5d794b8
Júlia Dornelas <129866827+Julia-Dornelas@users.noreply.github.com>
2023-08-03 07:27:12 -0300

    Add files via upload

---
+++ 1°Aula-JS/main.js
@@ -0,0 +1,17 @@
+console.log("Ola mundo, estou em um arquivo externo!")
+
+let a=3, b=15, c=6
+
+if(a > b){
+    if(a>c)
+    console.log(a)
+    else{
+        console.log(c)
+    }
+}
+else if(b>c){
+    console.log(b)
+}
+else{
+    console.log(c)
+}
