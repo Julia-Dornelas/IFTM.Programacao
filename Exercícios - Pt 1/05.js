b3fce1b5688f0dbea3b318fc8e6d6b047c342fa0
Júlia Dornelas <129866827+Julia-Dornelas@users.noreply.github.com>
2023-08-17 08:17:15 -0300

    Add files via upload

--- Exercícios - Pt 1/05.js
+++ Exercícios - Pt 1/05.js
@@ -13,19 +13,19 @@ parseFloat(altura)
 
 let imc = peso / (altura * altura)
 
-if(imc<=18,5){
+if(imc<=18.5){
     console.log("Abaixo do peso")
 }
-else if(imc>=18,6 && imc<=24,9){
+else if(imc>=18.6 && imc<=24.9){
 console.log("Peso ideal. Parabéns!")
 }
-else if(imc>=25 && imc<=29,9){
-    console.log("PeLevemente acima do peso")
+else if(imc>=25 && imc<=29.9){
+    console.log("Levemente acima do peso")
 }
-else if(imc>=30 && imc<=34,9){
+else if(imc>=30 && imc<=34.9){
     console.log("Obesidade grau I")
 }
-else if(imc>=35 && imc<=39,9){
+else if(imc>=35 && imc<=39.9){
     console.log("Obesidade grau II(severa)")
 }
 else if(imc>=40){
